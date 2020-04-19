df774e033f5c205b2d366ef2d7b88e54491a381e
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-04-18 22:19:05 -0400

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# timestamp-editor
+
+PowerShell script to edit file timestamps.
+
+## Function
+
+Each file / directory has three timestamps associated with it:
+- Creation time
+- Last modification time
+- Last access time
+
+Given a file, the script can:
+- Change any/each timestamp to a given time
+- Change all timestamps to a given time
+- Change all timestamps to match the timestamps of another given file
+
+Full file paths are to be provided (or, alternately, based on the file location of the script).
+
+This script has been tested on Windows 10. 
+
+## How to run
+
+1. Clone the repository via `git clone https://github.com/dulldesk/timestamp-editor.git`, or download and unzip it via `https://github.com/dulldesk/timestamp-editor/archive/master.zip`.
+2. Right-click `ts-edit-cli.ps1` > Run with PowerShell, or
+3. Open PowerShell
+4. Enter the full file path to the script (`C:\path\to\ts-edit-cli.ps1`)
+5. Follow the shell instructions.
+
+If running scripts is disabled on your system:
+- Enter `Set-ExecutionPolicy -scope CurrentUser bypass` into the shell. This will allow the script to run.
+- Try step 4 again.
+- You may wish to re-disable scripts once more by entering `Set-ExecutionPolicy -scope CurrentUser default`
---
+++ ts-edit-cli.ps1
@@ -0,0 +1,98 @@
+﻿function CheckQ {
+    param (
+         [Parameter(Mandatory = $true)]
+         $str
+    )
+    if ($str -eq 'q') {exit}
+}
+
+function FetchTime {
+    param (
+         [Parameter(Mandatory = $true)]
+         $tmename
+    )
+    
+    do {
+        $tme = read-host "enter" $tmename "time, or 'n' to skip"
+        CheckQ $tme
+        if ($tme.ToLower() -eq 'n') {
+            $tme = $false
+            break
+        }
+        $tme = $tme -as [DateTime]
+    } while (-not $tme)
+
+    return $tme
+}
+
+function FetchFile {
+    do {
+        $f = Read-Host "enter file path"
+        CheckQ $f
+        $f = $f.replace('"','')
+    } while (-not (Test-Path -path $f))
+    return $f
+}
+
+Write-Host "Timestamp Editor" -f cyan
+Write-Host "At any point, enter 'q' to exit"
+Write-Host " "
+
+while ($true) {
+    $file = Get-Item (FetchFile) -force
+    $file
+    Write-Host " "
+    
+    $copyfromfile = read-host "copy all timestamps from another file? [y/n]"
+    CheckQ $copyfromfile
+
+    if ($copyfromfile -eq 'y' -or $copyfromfile -eq 'Y') {
+        $other = Get-Item (FetchFile) -force
+        $file.CreationTime = $other.CreationTime
+        $file.LastWriteTime = $other.LastWriteTime
+        $file.LastAccessTime = $other.LastAccessTime
+
+    } else {
+        Write-Host ' '
+        Write-Host "Time format: DD month YYYY [HH:MM[:SS]]"
+        Write-Host ' '
+
+        $allsame = (read-host "make all timestamps equal? [y/n]").ToLower()
+        CheckQ $allsame
+
+        if ($allsame -eq 'q') {
+            $ts = FetchTime "timestamp"
+            $file.CreationTime = $ts
+            $file.LastWriteTime = $ts
+            $file.LastAccessTime = $ts
+        } 
+        else {
+            $times = @('creation','last write time','last access time')
+
+            for ($i=0;$i -lt $times.Length;$i++) {
+                $ts = FetchTime $times[$i];
+                if (-not $ts) {
+                    continue
+                }
+                if ($i -eq 0) {
+                    $file.CreationTime = $ts
+                }
+                elseif ($i -eq 1) {
+                    $file.LastWriteTime = $ts
+                }
+                elseif ($i -eq 2) {
+                    $file.LastAccessTime = $ts
+                }
+            }
+        }
+    }
+    Write-Host " "
+    Write-Host $file.name "timestamps" -f green
+    Write-host "creation time " $file.CreationTime
+    Write-host "last modification time " $file.LastWriteTime
+    Write-host "last access time " $file.LastAccessTime
+    Write-Host "  " 
+    $ask = Read-Host "continue? [y/n]"
+    if ($ask -ne 'y' -and $ask -ne 'Y') {break}
+    Write-Host " "
+}
